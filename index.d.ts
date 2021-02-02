304255e764b6c41fa7c64777b7962cfed39b5512
Ben Newman <ben@apollographql.com>
2021-02-01 20:19:45 -0500

    Support subclassing Observable with non-class constructor functions.
    https://github.com/apollographql/apollo-client/pull/7640#issuecomment-771272225

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,4 @@ import Observable = require("zen-observable");
 export { Observable };
 export type Observer<T> = ZenObservable.Observer<T>;
 export type Subscription = ZenObservable.Subscription;
+export type Subscriber<T> = ZenObservable.Subscriber<T>;
