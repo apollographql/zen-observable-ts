d6dc043088b5cfd6a4901a91961dc0ec06243e29
Ben Newman <ben@apollographql.com>
2021-07-08 17:48:01 -0400

    Compile for-of syntax in loose mode, like class syntax.

--- module.js
+++ module.js
@@ -1,4 +1,4 @@
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function () {}; return { s: F, n: function () { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function (e) { throw e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function () { it = it.call(o); }, n: function () { var step = it.next(); normalCompletion = step.done; return step; }, e: function (e) { didErr = true; err = e; }, f: function () { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+function _createForOfIteratorHelperLoose(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (it) return (it = it.call(o)).next.bind(it); if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; return function () { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
@@ -503,19 +503,10 @@ var Observable = /*#__PURE__*/function () {
           enqueue(function () {
             if (observer.closed) return;
 
-            var _iterator = _createForOfIteratorHelper(method.call(x)),
-                _step;
-
-            try {
-              for (_iterator.s(); !(_step = _iterator.n()).done;) {
-                var item = _step.value;
-                observer.next(item);
-                if (observer.closed) return;
-              }
-            } catch (err) {
-              _iterator.e(err);
-            } finally {
-              _iterator.f();
+            for (var _iterator = _createForOfIteratorHelperLoose(method.call(x)), _step; !(_step = _iterator()).done;) {
+              var item = _step.value;
+              observer.next(item);
+              if (observer.closed) return;
             }
 
             observer.complete();
--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,12 @@ const zenBabelPlugins = require("zen-observable/scripts/babel-plugins.js");
 const babelPlugins = zenBabelPlugins
   .filter(name => !name.startsWith("@babel/plugin-transform-modules"))
   .map(name => {
-    if (name.startsWith("@babel/plugin-transform-classes")) {
+    if (
+      name.startsWith("@babel/plugin-transform-classes") ||
+      name.startsWith("@babel/plugin-transform-for-of")
+    ) {
+      // These plugins compile to less code (without loss of functionality) if
+      // we enable "loose" mode.
       return [name, { loose: true }];
     }
     return name;
