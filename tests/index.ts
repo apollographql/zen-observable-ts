fdb2c6cdd43c5738c39c0eedb21f5b0c90428fab
Ben Newman <ben@apollographql.com>
2021-08-30 14:34:02 -0400

    Add `"type": "module"` to `package.json` and rename CommonJS modules (#156)

--- tests/index.ts
+++ tests/index.ts
@@ -1,6 +1,6 @@
 import test from "./Observable";
 
-const cjs = require("../index");
+const cjs = require("../index.cjs");
 describe("CommonJS modules", () => test(cjs.Observable));
 
 import * as esm from "../module";
